3d9c3171b8152813203c8d7e525585e54877948f
yaldakheirkhah <yal.kheirkhah@gmail.com>
2023-04-21 20:22:12 +0330

    adding filter to group

--- Self-Improvement/Hiiiiii/Program.cs
+++ Self-Improvement/Hiiiiii/Program.cs
@@ -2,7 +2,7 @@ using System.Security.Cryptography;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-var personGroup = app.MapGroup("/api/person");
+var personGroup = app.MapGroup("/api/person").CacheOutput().AddEndpointFilter<CustomHeaderFilter>();
 var person = new Person("yal");
 personGroup.MapGet("", () => new Person[] {person});
 personGroup.MapGet("{id:int}", (int id) => person);
@@ -16,4 +16,19 @@ public class Person
         Name = name;
     }
     public string Name { get; init; }
+}
+
+public class CustomHeaderFilter : IEndpointFilter
+{
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        if (context.HttpContext.Request.Headers.ContainsKey("X-My-Header"))
+        {
+            return await next(context);
+        }
+        else
+        {
+            return Results.Problem("no header !");
+        }
+    }
 }
