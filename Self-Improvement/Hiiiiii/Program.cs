94c0c9cf5daa85685c00d2e0f0c0e39b1f29b0c4
yaldakheirkhah <yal.kheirkhah@gmail.com>
2023-04-21 20:05:10 +0330

    some tests...

--- Self-Improvement/Hiiiiii/Program.cs
+++ Self-Improvement/Hiiiiii/Program.cs
@@ -1,6 +1,19 @@
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
+var personGroup = app.MapGroup("/api/person");
+var person = new Person("yal");
+personGroup.MapGet("", () => new Person[] {person});
+personGroup.MapGet("{id:int}", (int id) => person);
 
 app.Run();
+
+public class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; init; }
+}
